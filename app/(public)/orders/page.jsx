78d6aa3931fc7138abb2ee292865d3e7d6c8651f
rishabh.. <rishabhtripathi2004@gmail.com>
2025-11-05 20:28:08 +0530

    optimize

--- app/(public)/orders/page.jsx
+++ app/(public)/orders/page.jsx
@@ -16,7 +16,7 @@ export default function Orders() {
         <div className="min-h-[70vh] mx-6">
             {orders.length > 0 ? (
                 (
-                    <div className="my-20 max-w-7xl mx-auto">
+                    <div className="my-20 max-w-7xl mx-auto text-white">
                         <PageTitle heading="My Orders" text={`Showing total ${orders.length} orders`} linkText={'Go to home'} />
 
                         <table className="w-full max-w-5xl text-slate-500 table-auto border-separate border-spacing-y-12 border-spacing-x-4">
--- components/PageTitle.jsx
+++ components/PageTitle.jsx
@@ -7,7 +7,7 @@ const PageTitle = ({ heading, text, path = "/", linkText }) => {
         <div className="my-6">
             <h2 className="text-2xl font-semibold">{heading}</h2>
             <div className="flex items-center gap-3">
-                <p className="text-slate-600">{text}</p>
+                <p className="text-white">{text}</p>
                 <Link href={path} className="flex items-center gap-1 text-green-500 text-sm">
                     {linkText} <ArrowRightIcon size={14} />
                 </Link>
