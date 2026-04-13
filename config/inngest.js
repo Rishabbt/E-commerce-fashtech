aa8bbd7bc3b3f3646815ba57b83c42375eca2b7e
rishabh.. <rishabhtripathi2004@gmail.com>
2026-04-13 14:38:05 +0530

    fix theinngest fn syntax

--- config/inngest.js
+++ config/inngest.js
@@ -6,63 +6,52 @@ import User from "@/models/User";
 export const inngest = new Inngest({ id: "FashTech" });
 
 // Inngest function to save user data to a database
-
 export const syncUserCreation = inngest.createFunction(
     {
-        id:'sync-user-from-clerk'
-    },
-    {
-        event: 'clerk/user.created'
+        id: 'sync-user-from-clerk',
+        triggers: [{ event: 'clerk/user.created' }]  // ✅ moved here
     },
     async ({event}) => {
         const {id, first_name, last_name, email_addresses, image_url } = event.data
         const userData = {
-            _id:id,
+            _id: id,
             email: email_addresses[0].email_address,
             name: first_name + ' ' + last_name,
-            image_url:image_url
+            image_url: image_url
         }
         await connenctDB()
         await User.create(userData)
     }
 )
 
-// inngest fn to update user data in database
-
+// Inngest fn to update user data in database
 export const syncUserUpdation = inngest.createFunction(
     {
-        id: 'update-user-from-clerk'
-    },
-    {
-        event: 'clerk/user.updated'
+        id: 'update-user-from-clerk',
+        triggers: [{ event: 'clerk/user.updated' }]  // ✅ moved here
     },
     async ({event}) => {
-         const {id, first_name, last_name, email_addresses, image_url } = event.data
+        const {id, first_name, last_name, email_addresses, image_url } = event.data
         const userData = {
-            _id:id,
+            _id: id,
             email: email_addresses[0].email_address,
             name: first_name + ' ' + last_name,
-            image_url:image_url
+            image_url: image_url
         }
         await connenctDB()
         await User.findByIdAndUpdate(id, userData)
     }
 )
 
-// inngest fn to delete form DB
-
+// Inngest fn to delete from DB
 export const syncUserDeletion = inngest.createFunction(
     {
-        id:'delete-user-with-clerk'
-    },
-    {
-        event: 'clerk/user.deleted'
+        id: 'delete-user-with-clerk',
+        triggers: [{ event: 'clerk/user.deleted' }]  // ✅ moved here
     },
     async ({event}) => {
         const {id} = event.data
-
-            await connenctDB()
-            await User.findByIdAndDelete(id)
-        
+        await connenctDB()
+        await User.findByIdAndDelete(id)
     }
 )
