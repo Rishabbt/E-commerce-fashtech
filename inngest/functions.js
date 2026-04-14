ab8a8f289c108ef1cf5e740fabf4702add102e03
rishabh.. <rishabhtripathi2004@gmail.com>
2026-04-14 12:24:37 +0530

    fix the fn

--- inngest/functions.js
+++ inngest/functions.js
@@ -1,11 +1,20 @@
 import { inngest } from './client'
-import prisma from '@/lib/prisma';
+import { PrismaClient } from '@prisma/client'
+import { PrismaNeon } from '@prisma/adapter-neon'
+
+function getPrisma() {
+    const connectionString = process.env.DATABASE_URL
+    const adapter = new PrismaNeon({ connectionString })
+    return new PrismaClient({ adapter })
+}
+
 export const syncUserCreation = inngest.createFunction(
     {
         id: 'sync-user-from-clerk',
         triggers: [{ event: 'clerk/user.created' }]
     },
     async ({ event }) => {
+        const prisma = getPrisma()
         const { data } = event
         await prisma.user.create({
             data: {
@@ -24,6 +33,7 @@ export const syncUserUpdation = inngest.createFunction(
         triggers: [{ event: 'clerk/user.updated' }]
     },
     async ({ event }) => {
+        const prisma = getPrisma()
         const { data } = event
         await prisma.user.update({
             where: { id: data.id },
@@ -42,6 +52,7 @@ export const syncUserDeletion = inngest.createFunction(
         triggers: [{ event: 'clerk/user.deleted' }]
     },
     async ({ event }) => {
+        const prisma = getPrisma()
         const { data } = event
         await prisma.user.delete({
             where: { id: data.id }
