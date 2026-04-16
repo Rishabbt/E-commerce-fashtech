2e1c91175acc4bd2ddac870cfb359d02658c04d1
rishabh.. <rishabhtripathi2004@gmail.com>
2026-04-16 18:26:02 +0530

    add order fnc

--- components/OrderSummary.jsx
+++ components/OrderSummary.jsx
@@ -1,17 +1,19 @@
 import { PlusIcon, SquarePenIcon, XIcon } from 'lucide-react';
 import React, { useState } from 'react'
 import AddressModal from './AddressModal';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import toast from 'react-hot-toast';
 import { useRouter } from 'next/navigation';
 import { Protect, useAuth, useUser } from '@clerk/nextjs';
 import axios from 'axios';
+import { fetchCart } from '@/lib/features/cart/cartSlice';
 
 
 const OrderSummary = ({ totalPrice, items }) => {
     
     const {user} = useUser()
     const {getToken} =useAuth()
+    const dispatch = useDispatch()
     const currency = process.env.NEXT_PUBLIC_CURRENCY_SYMBOL || '₹';
 
     const router = useRouter();
@@ -43,7 +45,41 @@ const OrderSummary = ({ totalPrice, items }) => {
 
     const handlePlaceOrder = async (e) => {
         e.preventDefault();
+            try{
+                if(!user){
+                    return toast('Please login to place an order')
+                }
+                if(!selectedAddress){
+                    return toast('Please select an address')
+                }
+                const token = await getToken()
+                const orderData = {
+                    addressId: selectedAddress.id,
+                    items,
+                    paymentMethod
+                }
+                if(coupon){
+                    orderData.couponCode = coupon.code
+                }
+                // Create Order
 
+                const {data} = await axios.post('/api/orders', orderData, {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                })
+                if(paymentMethod === 'STRIPE'){
+                    window.location.href = data.session.url
+                } else {
+                    toast.success(data.message)
+                    router.push('/orders')
+                    dispatch(fetchCart({getToken}))
+                }
+
+            } catch (error){
+                toast.error(error?.response?.data?.error || error.message)
+
+            }
         router.push('/orders')
     }
 
